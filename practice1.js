b8af32737b83bd3f6eecf5d4699096a15fb75871
TracyWafula <tracywafula@gmail.com>
2026-03-30 20:47:02 +0200

    practice 1 and 2

---
+++ practice1.js
@@ -0,0 +1,7 @@
+//1.Personal Introduction Program 
+
+console.log("1.My name is Tracy Wafula.");
+console.log("2.I am 30 years old.");
+console.log("3.I come from Kenya.");
+console.log("4.Favourite food: Chicken Tikka Masala with Chapati");
+console.log("5.Javascript enables developer to build responsive user interfaces");
---
+++ practice2.js
@@ -0,0 +1,9 @@
+//2.Commented Program
+//Store name and age in independent variables and print both values 
+
+//Declare a variable and assign your name to is as a value 
+let lastName = "Wafula";
+//Declare a variable and assign your age to is as a value 
+let age = 30;
+//Print name and age
+console.log(`My name is ${lastName} and I am ${age} years old.`)
