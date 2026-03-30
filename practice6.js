ad00290b487387e7c460282b7aadb006715dff41
TracyWafula <tracywafula@gmail.com>
2026-03-30 23:11:16 +0200

    Partly did practice6

---
+++ practice6.js
@@ -0,0 +1,18 @@
+//Primitve vs Non-primitive Classification 
+
+//Primitive DataTypes 
+let employeeName = "Jonathan";// String
+const employeeID = 4376; // Number 
+let onLeave = false; //boolean
+var yearsofEmployment; //undefined
+let finalisedContracts = null // null 
+
+//Non-primitive Datatypes 
+let employmentStatus = {
+    contractType: "Permanent",
+    role: "Head of Service Department",
+    startDate: "02/06/2018"
+}
+
+const collegues = ["Richard", " Melissa", "Austine", "Grace"];
+
