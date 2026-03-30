754e76f6f046b51482b7294e27c9e202f1d7d57a
TracyWafula <tracywafula@gmail.com>
2026-03-30 22:04:08 +0200

    practice4-reassignment investigation

---
+++ practice4.js
@@ -0,0 +1,16 @@
+//Reassignment Investigation
+var studentName = "Claire Mbabazi";
+let studentID = 644606;
+const course = "Data Science";
+
+//Output variables before reassigning 
+console.log("Student details before reassignment");
+console.log(`Student, ${studentName}, ID Number ${studentID} is currenty enrolled in ${course}`)
+
+//Reassigning variables declared with var and let
+studentName = "Luisa Chege";
+studentID = 644232;
+console.log("Student details after reassignment");
+console.log(`Student, ${studentName}, ID Number ${studentID} is currenty enrolled in ${course}`)
+
+//Variables declared with const can't be redeclared or reassigned. 
