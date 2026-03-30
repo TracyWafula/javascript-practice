39ac8c08d2cff1863098c6c71dc20115c6b97808
TracyWafula <tracywafula@gmail.com>
2026-03-30 22:51:41 +0200

    practice4 Data Type Detective

---
+++ practice5.js
@@ -0,0 +1,13 @@
+//Data Type Detective
+var school = " Moringa School";
+let year = 2026;
+let enrollingStudents = false;
+const graduatedStudents = null;
+let  coursesOffered;
+//Illustrating data types for each variable declared
+console.log(`Variable :school Datatype: ${typeof school}`);
+console.log(`Variable : year Datatype: ${typeof year}`);
+console.log(`Variable : enrollingStudents Datatype: ${typeof enrollingStudents}`);
+console.log(`Variable :graduatedStudents Datatype:${graduatedStudents}`);
+console.log(`Variable :coursesOffered Datatype:${typeof coursesOffered}`);
+    
