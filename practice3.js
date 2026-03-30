c3a48d2ee0cff460ef7e6f29eb474d7b8a932ed0
TracyWafula <tracywafula@gmail.com>
2026-03-30 21:39:53 +0200

    practice2 questions

---
+++ practice3.js
@@ -0,0 +1,20 @@
+//3.Varible creation challenge.
+//String
+let schoolName = "MoringaSchool";
+//Number
+let coursesOffered= 5;
+//Boolen
+var enrollingStudent = true;
+//Array
+var modesOfStudy =  ["Remote","Part-time","Full-time", "In-person"];
+//object
+const contactDetails = {
+    mobileNumber: "+254711082146",
+    email: "contact@moringaschool.com"
+}
+//null
+const trainedStudents = null;
+
+
+
+
